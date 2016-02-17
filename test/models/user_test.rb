9902dafc9d20ae44569b8aa097b1c25ca61441c9
Ryan Collins <techrapportrc@gmail.com>
2016-02-17 20:39:46 +0000

    Feat: add user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@emample.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be preset" do
+    @user.name = "        "
+    assert_not @user.valid?
+  end
 end
