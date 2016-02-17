16bae6d7a293518f10225a12a7a5d245479eddd8
Ryan Collins <techrapportrc@gmail.com>
2016-02-17 04:48:24 +0000

    Fix: add Home route as root route in routes.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def full_title(page_title = '')
-        base_title = "Ruby on Rails Tests"
+        base_title = "Ruby on Rails Experiments"
         if page_title.empty?
             base_title
         else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tests"
+    assert_select "title", "Ruby on Rails Experiments"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tests"
+    assert_select "title", "Help | Ruby on Rails Experiments"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tests"
+    assert_select "title", "About | Ruby on Rails Experiments"
   end
-
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Experiments"
+  end
 end
