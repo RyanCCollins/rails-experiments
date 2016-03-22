492bf3b0d702f4eb4fdc7f1b77bb055d61fe1958
Ryan Collins <rccproductions@mac.com>
2016-03-22 07:45:51 -0400

    Feat: update with remember digest

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combo.'
@@ -15,5 +16,7 @@ class SessionsController < ApplicationController
     @current_user ||= User.find_by(id: session[:user_id])
   end
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
