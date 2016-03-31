8a2b3c7a83dd3a4bd6c50614479614376d2e693e
Ryan Collins <rccproductions@mac.com>
2016-03-30 23:23:16 -0400

    Feat: add successful login test to users_edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,11 +5,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:ryan)
   end
   test 'unsuccessful edit' do
+    login_as_user(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name: '',
                                     email: 'ryan@bah',
                                     password: 'foo',
                                     password_confirmation: 'bar' }
+  end
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: { name: 'Ryan Collins',
+                                    email: 'admin@ryancollins.io',
+                                    password: '',
+                                    password_confirmation: '' }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
